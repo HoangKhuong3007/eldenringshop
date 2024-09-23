422cfaef991b8e79a486e9632dfd4865dfb8530e
Nepteight <vpham2654@gmail.com>
2024-09-23 14:30:34 +0700

    Shop

---
+++ src/pages/public/Shop.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import styles
+import "../../styles/public/shop/shop.css";
+// import components
+import { Navbar } from "../../components/navbar/Navbar";
+export const Shop = () => {
+  return (
+    <div className="shop-container">
+      <Navbar />
+      <h2>Hello Shop</h2>
+    </div>
+  );
+};

386efaed72551b256bdcbffab8e613cf60418dee
Nepteight <vpham2654@gmail.com>
2024-09-29 20:56:08 +0700

    Change Code

--- src/pages/public/AboutUs.jsx
+++ src/pages/public/AboutUs.jsx
@@ -1,28 +1,38 @@
 import React from "react";
-import "./aboutus.css"; // Import the CSS file for styling
+import MenImg from "../../assets/Fashion-Accessories-Web-1.jpg";
+// import styles
+import "../../styles/public/aboutus/aboutus.css";
+// import components
+import { Navbar } from "../../components/navbar/Navbar";
+import { Footer } from "../../components/footer/Footer";
 
-const AboutUs = () => {
+export const AboutUs = () => {
   return (
     <div className="aboutus-container">
-      <h2 className="aboutus-heading">About Us</h2>
-      <div className="aboutus-content">
-        <p>
-          Welcome to Eldenring, your destination for modern men's fashion. We
-          specialize in crafting stylish, high-quality apparel and accessories
-          designed to elevate your look—whether you're dressing for work, play,
-          or special occasions.
-        </p>
-        <p>
-          At Eldenring, we believe that fashion is a reflection of who you are.
-          Our curated collections range from sharp tailored suits to casual
-          essentials, combining timeless designs with contemporary flair. Each
-          piece is made with attention to detail and quality craftsmanship,
-          ensuring you feel confident and comfortable.
-        </p>
-        <p>Join the Eldenring community and redefine your style with us.</p>
-      </div>
+      <Navbar />
+      <main className="aboutus-content">
+        <h2>About Us - EldenRing</h2>
+        <div className="aboutus-img">
+          <img src={MenImg} alt="" />
+        </div>
+        <div className="aboutus-text">
+          <p>
+            Welcome to Eldenring, your destination for modern men's fashion. We
+            specialize in crafting stylish, high-quality apparel and accessories
+            designed to elevate your look—whether you're dressing for work,
+            play, or special occasions.
+          </p>
+          <p>
+            At Eldenring, we believe that fashion is a reflection of who you
+            are. Our curated collections range from sharp tailored suits to
+            casual essentials, combining timeless designs with contemporary
+            flair. Each piece is made with attention to detail and quality
+            craftsmanship, ensuring you feel confident and comfortable.
+          </p>
+          <p>Join the Eldenring community and redefine your style with us.</p>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 };
-
-export default AboutUs;

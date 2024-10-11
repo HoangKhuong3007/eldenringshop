8a21f4368d6d0924f428bceaf9a87a7bc9bd27e2
HoangKhuong3007 <hoangkhuong2k4@gmail.com>
2024-10-11 10:53:46 +0700

    add profile menu

--- src/components/cartprofile/CartProfile.jsx
+++ src/components/cartprofile/CartProfile.jsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import "../../styles/components/cartprofile/cartprofile.css";
 
 export const CartProfile = () => {
+  const [showMenu, setShowMenu] = useState(false);
   const username = localStorage.getItem("username");
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const handleClickOutside = (event) => {
+    if (!event.target.closest(".cartprofile-icon")) {
+      setShowMenu(false);
+    }
+  };
+
+  React.useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="cartprofile-container">
-      <div className="cartprofile-icon">
+      <div className="cartprofile-icon" onClick={toggleMenu}>
         <FontAwesomeIcon icon={faCartShopping} />
       </div>
-      <div className="cartprofile-icon">
+      <div className="cartprofile-icon" onClick={toggleMenu}>
         <FontAwesomeIcon icon={faUser} />
         <span className="tooltip">{username}</span>
+        {showMenu && (
+          <div className="profile-menu">
+            <ul>
+              <li>Profile</li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
